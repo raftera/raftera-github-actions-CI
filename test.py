5b3a688134c7fa0666decb4b3c5b28934033e19e
Aaron Rafter <raftera@oregonstate.edu>
2021-06-04 15:15:43 -0700

    added subtract test and linted

--- test.py
+++ test.py
@@ -7,6 +7,9 @@ class TestCase(unittest.TestCase):
     def test_add_1(self):
         self.assertEqual(example.addition(1, 2), 3)
 
+    def test_subtract(self):
+        self.assertEqual(example.subtract(2, 1), 1)
+
 
 if __name__ == '__main__':
     unittest.main()
